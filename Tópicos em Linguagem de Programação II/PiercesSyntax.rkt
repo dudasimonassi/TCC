0b78d60871fa9df2dfdbdf1291fd6b26553f7d27
unknown <maria.simonassi@ice.ufjf.br>
2024-12-28 21:18:47 -0300

    Atualizações 28/12

---
+++ Tópicos em Linguagem de Programação II/PiercesSyntax.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(require redex)
+
+(provide (all-defined-out))
+
+; Definição da Linguagem Pierce´s em S-Expression
+;   T   →  true
+;       | false
+;       | if T then T else T
+;       | 0
+;       | succ T
+;       | pred T
+;       | iszero T
+
+; Definição da Linguagem Pierce´s
+(define-language Pierces
+    (T ::= 
+        (true)
+        (false)
+        (if T then T else T)
+        0
+        (succ T)
+        (pred T)
+        (iszero T)))
+
+; Testes e exemplos
+(redex-match? Pierces T (term (if (iszero 0) then (succ 0) else (pred 0)) ) ) 
